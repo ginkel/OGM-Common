e58a2038f59a0d49b809686cdba39e9f25fecb8f
Cornelius Köpp <cornelius.koepp@fhdw.de>
2023-02-13 21:53:35 +0100

    FlashStorage: `save()` Cleanup Structure of Module Data Writing

--- src/OpenKNX/FlashStorage.cpp
+++ src/OpenKNX/FlashStorage.cpp
@@ -175,7 +175,7 @@ namespace OpenKNX
             }
         }
 
-        // write magicword
+        // block of metadata
         _maxWriteAddress = _currentWriteAddress + FLASH_DATA_META_LEN;
 
         // application info
